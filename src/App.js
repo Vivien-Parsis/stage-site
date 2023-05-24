39fe66a3d48551e235501287f597ba995b33ca46
VIvien-Parsis <vivienparsis.perso@gmail.com>
2023-05-24 22:29:29 +0200

    init fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          test
-        </p>
+        <h1>
+          Cabinet d'implantologie dentaire du Dr Jean-Charles Giunta
+        </h1>
       </header>
     </div>
   );
